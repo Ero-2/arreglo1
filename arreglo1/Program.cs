c52f6f7f2c47412fee24bf3091cbb8e66c772f3c
erick garcia <erick garcia@DESKTOP-QFHIISE>
2023-03-14 16:33:30 -0600

    Agregar archivos de proyecto.

---
+++ arreglo1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+            int suma = 0;
+            for (int i = 0; i < valores.Length; i++)
+            {
+                suma += valores[i];
+            }
+            Console.WriteLine("La suma de los valores del arreglo es: {0}", suma);
+        }
+    }
+}
